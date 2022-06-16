66b3e6320936b7af29ddd404e1ae24f06b728792
targetfaang <104792336+targetfaang@users.noreply.github.com>
2022-06-16 23:31:58 +0530

    https://leetcode.com/problems/contains-duplicate/
    https://leetcode.com/problems/contains-duplicate/
    Time complexity  : o(n2)
    Space complexity : o(1)

---
+++ contains-duplicate.py
@@ -0,0 +1,11 @@
+https://leetcode.com/problems/contains-duplicate/
+
+code : 
+  
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]==nums[j]:
+                    return True
+        return False
