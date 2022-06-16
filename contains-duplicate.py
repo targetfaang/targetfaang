00e66cc5a0509de0cd3e58472793bc0a3cf2dd98
targetfaang <104792336+targetfaang@users.noreply.github.com>
2022-06-17 01:06:42 +0530

    Update contains-duplicate.py

--- contains-duplicate.py
+++ contains-duplicate.py
@@ -36,3 +36,16 @@ Sorting --> Time O(n log n) / Memory O(1)
         return False
       
    tc-o(n) sc-o(1)
+  
+  
+  94.9 % faster 
+  
+  class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hash_set = set()
+        for i in range(len(nums)):
+            if nums[i] in hash_set:
+                return True
+            else:
+                hash_set.add(nums[i])
+        return False
